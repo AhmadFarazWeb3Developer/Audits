3033230dc00099ce81e6084f6ddc917247469c31
AhmadFarazWeb3Developer <ahmadfaraz.web3.0.developer@gmail.com>
2025-07-19 11:50:18 +0500

    Added Aderyn analysis report and refactor Solidity imports for consistency

--- 5-boss-bridge/src/L1Token.sol
+++ 5-boss-bridge/src/L1Token.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract L1Token is ERC20 {
+    // ok
     uint256 private constant INITIAL_SUPPLY = 1_000_000;
 
     constructor() ERC20("BossBridgeToken", "BBT") {
